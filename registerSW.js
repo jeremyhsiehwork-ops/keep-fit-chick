ca1022318ca2726829d904578f152cad03bfe4d5
jeremyhsiehwork-ops <jeremyhsiehwork@gmail.com>
2026-03-05 01:04:33 +0800

    debug the file not found

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/keep-fit-chick/sw.js', { scope: '/keep-fit-chick/' })})}
